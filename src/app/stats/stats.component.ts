a9c07a0a45991c6eda8b5410f46eeb4d771bda9e
Romain Baumier <baumier.romain@gmail.com>
2017-06-27 10:51:35 +0200

    feat(websocket) + show progress

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -4,12 +4,12 @@ import * as _ from 'lodash';
 
 // Implémentation de l'interface de Stats avec ses attributs
 interface Stats {
-  id: String;
+  _id: String;
   name: String;
   uploadedAt: Date;
   uploadDuration: Number;
-  status: String
-  url: String
+  status: String;
+  url: String;
 }
 
 // Creation du composant
@@ -23,28 +23,35 @@ export class StatsComponent {
   // attributs exortés
   stats: Array<Stats>; // tableau de stats
   isAscending: Boolean; // variable de tri défini
+  handlers = {
+    progress({ _id, progress }) {
+      const stat = _.find(this.stats, { _id });
+      (<Stats>stat).status = `${progress}%`;
+    },
+
+    stats(stats) {
+      this.stats = stats;
+      this.sortByDate();
+    }
+  };
 
   // construction des services de l'API
   constructor(
     private statsService: StatsService
   ) { }
 
-
   // Fonction de stats
-
   ngOnInit() {
-    this.statsService.findStats() // le fonction Find est appelé
-      .subscribe( // souscription
-      (stats) => {
-        this.stats = stats;
-        this.sortByDate();
-      }, // retour = stats
-      (err) => console.error(err) // sinon erreur
-      );
+    this.initEvents();
+  }
+
+  initEvents() {
+    this.statsService.eventCallback$.subscribe(([event, payload]) => {
+      this.handlers[event].call(this, payload);
+    });
   }
 
   // Fonction de tri
-
   sortByDate() {
     this.isAscending = !this.isAscending;
     this.stats = _.orderBy(this.stats, 'uploadedAt', this.isAscending ? 'desc' : 'asc');
--- src/app/stats/stats.services.ts
+++ src/app/stats/stats.services.ts
@@ -1,25 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import * as Nes from 'nes';
 
 
 // Appel des routes de l'API
 @Injectable()
 export class StatsService {
-  baseUrl = 'http://localhost:8086';
+  private eventCallback: Subject<any[]> = new Subject<any>();
+  eventCallback$ = this.eventCallback.asObservable();
 
-  constructor(private http: Http) { }
-
-  // Route de récupération de tous les animaux
-  findStats() {
-    return this.http.get(`${this.baseUrl}/videos/stats`)
-      .map(this.extractData); // si quelque chose est attendu en retour de la méthode appelé par la route
+  constructor(private http: Http) {
+    var client = new Nes.Client('ws://localhost:8086');
+    client.connect((err) => {
+      if(err) {
+        return console.error(`ERROR during websocket connection: ${err}`);
+      }
+      this.initWebsocketHandlers(client);
+    });
   }
 
-  //Conversion Http ->json
-  private extractData(res: Response) {
-    return res.json();
+  initWebsocketHandlers(client) {
+    client.request('/videos/stats', (err, payload) => {
+       this.eventCallback.next(['stats', payload]);
+    });
+    client.subscribe('/videos/progress', (status, flags) => {
+      if(status.isTrusted) {
+        status = JSON.parse(status.data).message;
+      }
+      this.eventCallback.next(['progress', status]);
+    }, (err) => {
+
+    });
   }
 }
